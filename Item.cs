db70674c0d895efa0863da30231b7fd6b3bf57f7
KRISHNAKUMAR CHINTHAKUNTA <150474233+Krishna575@users.noreply.github.com>
2026-03-25 00:18:03 +0530

    Add files via upload
    Initial commit - ERP Item Processing System

---
+++ Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ERPItemProcessor.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Weight { get; set; }
+
+        public int? ParentId { get; set; }
+    }
+}
---
+++ ItemsController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ERPItemProcessor.Data;
+using ERPItemProcessor.Models;
+
+namespace ERPItemProcessor.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private AppDbContext db = new AppDbContext();
+
+        // GET: Items
+        public ActionResult Index()
+        {
+            return View(db.Items.ToList());
+        }
+
+        // GET: Items/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = db.Items.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+        // GET: Items/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Items/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Weight,ParentId")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Items.Add(item);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(item);
+        }
+
+        // GET: Items/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = db.Items.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+        // POST: Items/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Weight,ParentId")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(item).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(item);
+        }
+
+        // GET: Items/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = db.Items.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            return View(item);
+        }
+
+        // POST: Items/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Item item = db.Items.Find(id);
+            db.Items.Remove(item);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // 🔥 PROCESS ITEM (GET)
+        public ActionResult Process()
+        {
+            ViewBag.Items = db.Items.ToList();
+            return View();
+        }
+
+        // 🔥 PROCESS ITEM (POST)
+        [HttpPost]
+        public ActionResult Process(int parentId, string childName, double childWeight)
+        {
+            Item child = new Item
+            {
+                Name = childName,
+                Weight = childWeight,
+                ParentId = parentId
+            };
+
+            db.Items.Add(child);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        // ⭐ TREE VIEW (GET)
+        public ActionResult Tree()
+        {
+            var items = db.Items.ToList();
+            return View(items);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
